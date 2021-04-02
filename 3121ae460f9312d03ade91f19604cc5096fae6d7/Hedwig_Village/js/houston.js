521fffd433920a408018f37f88e22f9bc6423a51
Crobertsonrealtor <80546310+Crobertsonrealtor@users.noreply.github.com>
2021-04-02 15:20:43 -0500

    thicken neighborhood boundary lines

--- 3121ae460f9312d03ade91f19604cc5096fae6d7/Hedwig_Village/js/houston.js
+++ 3121ae460f9312d03ade91f19604cc5096fae6d7/Hedwig_Village/js/houston.js
@@ -1357,7 +1357,7 @@ map.on('load', function () {
       "source": "route5",
       "type": "line",
       "paint": {
-      "line-width": 0.5,
+      "line-width": 1,
       "line-color": "red"
       },
       layout: {
